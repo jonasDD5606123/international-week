0aef223c3bcd11ca910881fadbd014e76780738b
ismail750 <ismail.zitouni@student.odisee.be>
2025-04-22 13:27:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
 from config import Config
 
 app = Flask(__name__)
@@ -12,4 +11,3 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 app.config.from_object(Config)
-mysql = MySQL(app)
