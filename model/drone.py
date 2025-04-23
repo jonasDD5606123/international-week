d75d3e2be419813245baa564d681db70996cbd27
jonasDD5606123 <115703340+jonasDD5606123@users.noreply.github.com>
2025-04-23 10:11:56 +0200

    Update drone.py

--- model/drone.py
+++ model/drone.py
@@ -9,10 +9,27 @@ class Drone:
 
     def create(self):
         # Use parameterized query to avoid SQL injection
-        sql = '''INSERT INTO drones (batterijlevel, isbeschikbaar) 
-                 VALUES (?, ?)'''
+        sql = '''insert into drones (batterijlevel, isbeschikbaar) 
+                 values (?, ?)'''
         dc = DatabaseContext()
         conn = dc.getDbConn()
         cursor = conn.cursor()
         cursor.execute(sql, (self.batterijLevel, self.beschikbaarheid))
         conn.commit()
+
+    def update(self):
+        sql = '''update drones where id = ?'''
+        dc = DatabaseContext()
+        conn = dc.getDbConn()
+        cursor = conn.cursor()
+        cursor.execute(sql (self.id))
+        conn.commit()
+
+    @staticmethod
+    def all(self):
+        sql = 'select * from drones'
+        dc = DatabaseContext()
+        conn = dc.getDbConn()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()
