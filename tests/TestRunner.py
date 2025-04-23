eb53865dbef509da143fe520f3959400e489b5df
JuliaanPetitjean <juliaan.petitjean@student.odisee.be>
2025-04-23 16:46:11 +0200

    Delete TestRunner.py

--- tests/TestRunner.py
+++
@@ -1,9 +0,0 @@
-# TestRunner.py
-import subprocess
-
-def run_behave():
-    result = subprocess.run(["behave", "features/"], capture_output=True, text=True)
-    print(result.stdout)
-
-if __name__ == "__main__":
-    run_behave()
